25c845fdc12d2206c7b5122d22c4780ae6a0459e
OUWANG <wangou@bc.edu>
2018-09-02 13:50:09 -0400

    fresh tomatoes website

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -1,3 +1,31 @@
+import fresh_tomatoes
 import media
 
-toy_story = media.Movie()
+toy_story_3 = media.Movie("Toy Story 3",
+                        "A story of a boy and his toys come to life",
+                        "https://upload.wikimedia.org/wikipedia/en/6/69/Toy_Story_3_poster.jpg",
+                        "https://www.youtube.com/watch?v=JcpWXaA2qeg")
+avatar = media.Movie("Avatar",
+                        "A marine on an alien planet",
+                        "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                        "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+crazy_rich_asians = media.Movie("Crazy Rich Asians",
+                              "A love story of a girl to be marrying into the rich asian family",
+                              "https://upload.wikimedia.org/wikipedia/en/b/ba/Crazy_Rich_Asians_poster.png",
+                              "https://www.youtube.com/watch?v=ZQ-YX-5bAs0")
+mission_impossible_fallout = media.Movie("Mission: Impossbile - Fallout",
+                                         "Doing all kinds of cool shit impossible",
+                                         "https://upload.wikimedia.org/wikipedia/en/f/ff/MI_%E2%80%93_Fallout.jpg",
+                                         "https://www.youtube.com/watch?v=XiHiW4N7-bo")
+ratatouille = media.Movie("Ratatouille",
+                          "Rat cooking French cuisine",
+                          "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+sausage_party = media.Movie("Sausage Party",
+                            "Food coming to life",
+                            "https://upload.wikimedia.org/wikipedia/en/e/e4/Sausage_Party.png",
+                            "https://www.youtube.com/watch?v=9VoNgLnjzVg")
+
+movies = [toy_story_3, avatar, crazy_rich_asians, mission_impossible_fallout, ratatouille, sausage_party]
+
+fresh_tomatoes.open_movies_page(movies)
--- movies/media.py
+++ movies/media.py
@@ -10,14 +10,22 @@ Created on Sat Sep  1 21:23:31 2018
 Notes:
     
 The double underscore is python telling us that this function is reserved, a special function or method
-class
-instance
-constructor
+class - a blue print with both data and methods
+instance - (toy story, avatar)
+constructor - the init function
 self - the object being created
+instance variables - (titile, storyline, etc)
+instance method - functions within class
 """
+import webbrowser
+
 class Movie():
     def __init__(self, movie_title, movie_storyline, poster_image,trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+    def show_poster(self):
+        webbrowser.open(self.poster_image_url)
